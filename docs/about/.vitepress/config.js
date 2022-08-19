3ba6bc140985c8425a21ab2196a6533833f50023
Hossam Rashad <learncodingeasy@yahoo.com>
2022-08-19 19:44:07 +0200

    contact

--- docs/about/.vitepress/config.js
+++ docs/about/.vitepress/config.js
@@ -1,14 +1,33 @@
+let ourStorySidebar = [{ title: "/Our Story", link: "/about/our-story" }];
+
 module.exports = {
+  lang: "en-US",
+  // Title Page
   title: "VitePress",
+  // Page Description
+  description: "Vite & Vue powered static site generator.",
 
   // Theme related configurations.
   themeConfig: {
-    logo: "/logo.svg",
+    // logo: "/logo.svg",
     nav: [
       { title: "/Home", link: "/" },
       { title: "/About", link: "/about" },
       { title: "/Contact", link: "/contact" },
     ],
-    sidebar: [{ title: "/Our Story", link: "/about/our-story" }],
+    sidebar: {
+      "/about": ourStorySidebar,
+      "/contact": ourStorySidebar,
+    },
   },
+  // appearance: false,
+  // export default {
+  head: [
+    [
+      "link",
+      { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
+    ],
+    // would render: <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  ],
+  // }
 };
--- docs/index.md
+++ docs/index.md
@@ -1,7 +1,25 @@
 # Hello World
 
+## Header
+### Routes
+
 [contact](/contact)
 
 [contact](/contact.md)
 
 [contact](/contact.html)
+
+## Code
+
+```js
+console.log(`Yes`);
+```
+## Table Of Contents
+[[toc]]
+
+# Tables
+
+| Heading    | Colum 2 | Colum 3
+| :-----:    | :-----: | :-----: |
+| Lorem Ipson|  Top    |  Lift   | 
+| Lorem      |  Top    |  Right  | 
