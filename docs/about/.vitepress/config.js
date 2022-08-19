85bcab26fb3c1cf82d3196c0458b307f40f7780c
Hossam Rashad <learncodingeasy@yahoo.com>
2022-08-19 19:14:59 +0200

    Theme related configurations.

--- docs/about/.vitepress/config.js
+++ docs/about/.vitepress/config.js
@@ -1,3 +1,14 @@
 module.exports = {
   title: "VitePress",
+
+  // Theme related configurations.
+  themeConfig: {
+    logo: "/logo.svg",
+    nav: [
+      { title: "/Home", link: "/" },
+      { title: "/About", link: "/about" },
+      { title: "/Contact", link: "/contact" },
+    ],
+    sidebar: [{ title: "/Our Story", link: "/about/our-story" }],
+  },
 };
